14c69fb621d623258be0078f094df5c9943605f1
Kacper Lobodecki <kaspereo@gmail.com>
2021-11-17 14:06:55 +0100

    Add and read new data file

--- Projekt1/proj1.m
+++ Projekt1/proj1.m
@@ -2,7 +2,7 @@ clear;
 a=6378137; 
 e2=0.00669437999013; 
 %wsp samolotu 
-macierzDane=load('daneMoje1.txt'); 
+macierzDane=load('daneMoje2.txt'); 
 phi=macierzDane(:,1);  
 lambda=macierzDane(:,2); 
 h=macierzDane(:,3); 

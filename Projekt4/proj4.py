c629599e5668f14ed323868340e043ceb28735ea
Kacper Lobodecki <kaspereo@gmail.com>
2022-01-19 23:19:00 +0100

    Project 4 init

---
+++ Projekt4/proj4.py
@@ -0,0 +1,22 @@
+import math as m
+from shapely.geometry import Polygon
+
+a=6378137; 
+e2=0.00669437999013;
+
+pktA = [50.25,  20.75]
+pktB = [50,     20.75]
+pktC = [50.25,  21.25]
+pktD = [50,     21.25]
+
+def fromGeoTo1992():
+  return 0
+
+def fromGeoTo2000():
+  return 0
+  
+def from1992ToGeo():
+  return 0
+  
+def from2000toGeo():
+  return 0
